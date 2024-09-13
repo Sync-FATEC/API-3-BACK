8ba63ec07c5129eab3b0fa576a1d8dc2708077f1
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-09-13 12:46:53 -0300

    refactor: atualizei nome de variavel

--- src/main/java/com/sync/api/dto/projetcs/CoordinatorDTO.java
+++ src/main/java/com/sync/api/dto/projetcs/CoordinatorDTO.java
@@ -1,13 +1,13 @@
 package com.sync.api.dto.projetcs;
 
-import com.sync.api.model.Usuario;
+import com.sync.api.model.User;
 
 public class CoordinatorDTO {
     public String id;
     public String name;
-    public Usuario user;
+    public User user;
 
-    public CoordinatorDTO(String id, String name, Usuario user) {
+    public CoordinatorDTO(String id, String name, User user) {
         this.id = id;
         this.name = name;
         this.user = user;
--- src/main/java/com/sync/api/infra/security/TokenService.java
+++ src/main/java/com/sync/api/infra/security/TokenService.java
@@ -4,8 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.sync.api.model.Usuario;
-import org.springframework.beans.factory.annotation.Value;
+import com.sync.api.model.User;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -17,7 +16,7 @@ public class TokenService {
 
     private final String secret = "segredo";
 
-    public String generateToken(Usuario usuario){
+    public String generateToken(User usuario){
         try{
             Algorithm algorithm = Algorithm.HMAC256(secret);
             String token = JWT.create()
