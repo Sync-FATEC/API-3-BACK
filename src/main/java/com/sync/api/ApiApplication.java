cc0fb2cf23951ffe1a25ce69acb3e115ae42ee81
Manhã <joaoggbs62@gmail.com>
2024-09-13 07:57:36 -0300

    feat: Adicionando método para execução do script de inserção de dados no banco.

--- src/main/java/com/sync/api/ApiApplication.java
+++ src/main/java/com/sync/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.sync.api;
 
+import com.sync.api.controller.RunPythonScript;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+		RunPythonScript.main(new String[]{});
 	}
 
 }
