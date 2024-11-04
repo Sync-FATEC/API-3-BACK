21fe97be2baeab2cd61d453463c3cf704ba3c987
Elisa Rachel <elisarachel2@gmail.com>
2024-11-04 07:48:08 -0300

    feat: Adicionei funcionalidade de exportação do dashboard para pdf e excel

--- src/main/java/com/sync/api/controller/DashboardController.java
+++ src/main/java/com/sync/api/controller/DashboardController.java
@@ -9,10 +9,13 @@ import com.sync.api.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -121,6 +124,34 @@ public class DashboardController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao calcular o investimento: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/export/{format}")
+    public ResponseEntity<byte[]> exportDashboard(
+            @PathVariable("format") String format,
+            @RequestParam(required = false) String nameCoordinator,
+            @RequestParam(required = false) String startDate,
+            @RequestParam(required = false) String endDate) {
+        try {
+            byte[] fileData;
+            HttpHeaders headers = new HttpHeaders();
+
+            if ("pdf".equalsIgnoreCase(format)) {
+                fileData = dashboardService.exportDashboardToPDF(nameCoordinator, startDate, endDate);
+                headers.setContentType(MediaType.APPLICATION_PDF);
+                headers.setContentDispositionFormData("inline", "dashboard_report.pdf");
+            } else if ("excel".equalsIgnoreCase(format)) {
+                fileData = dashboardService.exportDashboardToExcel(nameCoordinator, startDate, endDate);
+                headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+                headers.setContentDispositionFormData("attachment", "dashboard_report.xlsx");
+            } else {
+                return ResponseEntity.badRequest().body("Formato inválido: use 'pdf' ou 'excel'.".getBytes());
+            }
+
+            return new ResponseEntity<>(fileData, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao exportar dados do dashboard".getBytes());
         }
     }
 }
